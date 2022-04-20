38a695ce285c887f268451a791e39c9e78cfae91
Linus Hochbaum <linus.hochbaum@bridgefield.de>
2022-04-20 11:21:31 +0200

    Initial commit

---
+++ addon.py
@@ -0,0 +1,37 @@
+from mitmproxy import ctx, http
+from mitmproxy import flowfilter
+import json
+
+METHOD = "PUT"
+ENDPOINT = "https://dibe.services.ihk.de/berichtsheft/erstellen-api/v1/berichtswoche"
+SKILLS = [5, 4, 11, 1, 10, 12, 27]
+
+
+class IHKrus:
+    def __init__(self):
+        self.filter: flowfilter.TFilter = flowfilter.FMethod("PUT")
+
+    def request(self, flow: http.HTTPFlow) -> None:
+        if not flowfilter.match(self.filter, flow):
+            return
+
+        if flow.request.url == ENDPOINT:
+            data = flow.request.json()
+            daily_reports = data["tagesBerichte"]
+
+            for report in daily_reports:
+                entries = report["eintraege"]
+
+                for entry in entries:
+                    skills = entry["qualifikationen"]
+                    skills.clear()
+
+                    for skill in SKILLS:
+                        skills.append({"berufsbildPositionId": skill})
+
+            flow.request.content = json.dumps(data).encode()
+            ctx.master.commands.call("replay.client", [flow])
+            ctx.log.info("IHKrus: You are one qualified man!")
+
+
+addons = [IHKrus()]
